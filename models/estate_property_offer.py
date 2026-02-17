d94add87b74bdb455153fad12b4ed001367f4e5e
Omar Flores <oafo2699@gmail.com>
2026-02-17 17:31:28 -0500

    cambio de variable de partenr a partner

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -3,7 +3,6 @@ from odoo import models, fields
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
     _description = 'Estate Property Offer Model'
-
     price = fields.Float(string="Price",digits=(16,2))
     state = fields.Selection(
         [
@@ -15,7 +14,7 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one(
         "res.partner",
-        string="Partenr",
+        string="Partner",
         required=True
     )
 
@@ -24,4 +23,3 @@ class EstatePropertyOffer(models.Model):
         string="Estate Property",
         required=True
     )
-
