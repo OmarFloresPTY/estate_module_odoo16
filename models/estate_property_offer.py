ed52adae79320028bdb2e6f00ad6fbb5dedebe08
Omar Flores <oafo2699@gmail.com>
2026-03-09 00:10:56 -0500

    feature: campo calculado e inverso en fecha habilitada

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -12,6 +13,26 @@ class EstatePropertyOffer(models.Model):
         string="State"
     )
 
+    validity = fields.Integer(string="Validity", default=7)
+    date_deadline = fields.Date(string="Date Deadline", compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            # Si el registro es nuevo, create_date es None. Usamos la fecha de hoy.
+            start_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = start_date + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            # Aseguramos que tratamos con objetos de fecha (Date)
+            create_date = fields.Date.to_date(record.create_date) if record.create_date else fields.Date.today()
+            if record.date_deadline and create_date:
+                # La resta de dos objetos Date devuelve un timedelta de Python
+                delta = record.date_deadline - create_date
+                record.validity = delta.days
+
+
     partner_id = fields.Many2one(
         "res.partner",
         string="Partner",
@@ -23,3 +44,4 @@ class EstatePropertyOffer(models.Model):
         string="Estate Property",
         required=True
     )
+
